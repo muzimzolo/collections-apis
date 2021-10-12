c1b6c06215dec705831f5b54ff936f9313b85843
muzimzolo <muzisoftwaredev@gmail.com>
2021-10-12 10:21:44 +0200

    create collection for hotel rooms

--- src/com/debugger/collections/api/Application.java
+++ src/com/debugger/collections/api/Application.java
@@ -1,9 +1,23 @@
 package com.debugger.collections.api;
+import com.debugger.collections.api.Room;
+import java.util.*;
 
 public class Application {
 	
-	/*
-	 * Insert implementation
-	 */
-
+	public static void main(String[] args) {
+		Room flamingo = new Room("Flamingo","Premiere Room", 4, 1750.00);
+		Room grayElephant = new Room("Gray Elephant","Suite", 5, 2500.00);
+		Room amur = new Room("Amur","Guest Room", 3, 1250.00);
+		Room pineapple = new Room("Pineapple","Suite", 6, 2250.00);
+		
+		Collection <Room> rooms = List.of(flamingo, grayElephant, amur, pineapple);
+		double total = getPotentialReturn(rooms);
+		System.out.println("total potential revenue: R" + total);
+	}
+	
+	private static double getPotentialReturn(Collection<Room> rooms) {
+		return rooms.stream()
+				.mapToDouble(r -> r.getRate()).sum();
+		
+	}
 }
